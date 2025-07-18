18f2cdd58af84421ea05e47f7167f6a0c6fc4d20
Rajesh Narayana Rao <mailrbn@gmail.com>
2025-07-18 16:10:21 +1000

    First Committ

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Demo
+{
+    class Data
+    { 
+        public void Rajesh()
+        {
+            Console.WriteLine("Hello Rajesh");
+        }
+        internal void Narayana()
+        {
+            Console.WriteLine("Hello Narayana");
+        }
+        private void Rao()
+        {
+            Console.WriteLine("Hello Rao");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data data = new Data();
+            data.Rajesh();
+            data.Narayana();
+            // data.Rao(); // This will cause a compile-time error since Rao is private
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to dotnet programming");
+
+            Console.WriteLine("Enter your name     ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
